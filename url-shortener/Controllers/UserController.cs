2e1a77e59f949e6af807b139e92d354abfc30844
Lucas Gandossini <lucas.gandossini@outlook.com>
2023-11-01 19:48:22 -0300

    Http codes hotfix

--- url-shortener/Controllers/UserController.cs
+++ url-shortener/Controllers/UserController.cs
@@ -20,14 +20,14 @@ namespace url_shortener.Controllers
         public IActionResult GetAll()
         {
             string userRole = User.Claims.First(claim => claim.Type.Contains("role")).Value;
-            if (userRole != "Admin") return Forbid("Unauthorized");
+            if (userRole != "Admin") return Forbid();
             return Ok(_service.GetAll());
         }
         [HttpGet("admin/users/{id}")]
         public IActionResult GetById(int id)
         {
             string userRole = User.Claims.First(claim => claim.Type.Contains("role")).Value;
-            if (userRole != "Admin") return Forbid("Unauthorized");
+            if (userRole != "Admin") return Forbid();
             UserDto? user = _service.GetById(id);
             if (user is not null) return Ok(user);
             return NotFound("User not found");
@@ -47,7 +47,7 @@ namespace url_shortener.Controllers
             string userRole = User.Claims.First(claim => claim.Type.Contains("role")).Value;
             string email = User.Claims.First(claim => claim.Type.Contains("email")).Value;
             Console.WriteLine(userRole);
-            if (userRole != "Admin" && email != dto.Email) return Forbid("Unauthorized");
+            if (userRole != "Admin" && email != dto.Email) return Forbid();
             _service.Update(dto);
             return NoContent();
         }
@@ -57,7 +57,7 @@ namespace url_shortener.Controllers
             string userRole = User.Claims.First(claim => claim.Type.Contains("role")).Value;
             string email = User.Claims.First(claim => claim.Type == ClaimTypes.Email).Value;
             Console.WriteLine("Delete used by:", userRole);
-            if (userRole != "Admin" && email != dto.Email) return Forbid("Unauthorized");
+            if (userRole != "Admin" && email != dto.Email) return Forbid();
             if (!_service.Exists(dto.Email)) return NotFound("User not found");
             _service.Delete(dto);
             return NoContent();
