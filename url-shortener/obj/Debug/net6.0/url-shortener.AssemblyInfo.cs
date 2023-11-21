a61c91bd73fdfa56fbec13053cc4d36f0f8814b5
Lucas Gandossini <lucas.gandossini@outlook.com>
2023-11-21 18:40:29 -0300

    bin + obj + db commit

--- url-shortener/obj/Debug/net6.0/url-shortener.AssemblyInfo.cs
+++ url-shortener/obj/Debug/net6.0/url-shortener.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("url-shortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a9e884cd14c1d1fdc9f4a6a584f627dab0cf99f")]
 [assembly: System.Reflection.AssemblyProductAttribute("url-shortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("url-shortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
